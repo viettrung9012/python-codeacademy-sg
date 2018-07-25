9e39e24fe9d5bb8292818e88e6d40ba63f75e625
Viet Trung Truong <vitruong@SINC02W40C0G8WL.sea.corp.expecn.com>
2018-07-25 16:15:27 +0800

    Add certificate pdf

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "alias python=python3" > /home/nbuser/.bash_aliases
+
+# Install dependencies for Python 3
+source /home/nbuser/anaconda3_420/bin/activate
+pip install msgpack reportlab pypdf2
+
+# Install dependencies for Python 3.6
+source /home/nbuser/anaconda3_501/bin/activate
+pip install msgpack reportlab pypdf2
